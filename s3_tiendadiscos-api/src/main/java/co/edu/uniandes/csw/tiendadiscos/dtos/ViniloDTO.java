a32bf25d62d6216394067f4894cfd9ac0889b61c
estudiante <estudiante@ISIS2603-303.sis.virtual.uniandes.edu.co>
2018-08-26 19:10:54 -0500

    Clase de vinilo con los gets (falta documentación).

--- s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/ViniloDTO.java
+++ s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/ViniloDTO.java
@@ -7,13 +7,23 @@ package co.edu.uniandes.csw.tiendadiscos.dtos;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
- * @author af.hernandezl
+ * @author estudiante
  */
-public class ViniloDTO implements Serializable
-{
+@Entity
+public class ViniloDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     /**
      * Nombre del vinilo.
      */
@@ -43,6 +53,61 @@ public class ViniloDTO implements Serializable
      * 
      */
     private String previewURI;
-    
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public Date getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public String getProductora() {
+        return productora;
+    }
+
+    public String getInformacionAdicional() {
+        return informacionAdicional;
+    }
+
+    public String getPreviewURI() {
+        return previewURI;
+    }  
+ 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ViniloDTO)) {
+            return false;
+        }
+        ViniloDTO other = (ViniloDTO) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "co.edu.uniandes.csw.tiendadiscos.dtos.ViniloDTO[ id=" + id + " ]";
+    }    
 }
