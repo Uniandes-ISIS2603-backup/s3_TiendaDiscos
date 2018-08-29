b0dfc8dcdc7ee15ea252a054ae1c5df9ce2e71cb
Kevin Blanco <>
2018-08-29 01:09:59 -0500

    DTOS, DETAIl y ENTITY para billing y tarjeta de credito parece que esta todo terminado

--- s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/BillingInformationDetailDTO.java
+++ s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/BillingInformationDetailDTO.java
@@ -5,10 +5,61 @@
  */
 package co.edu.uniandes.csw.tiendadiscos.dtos;
 
+import co.edu.uniandes.csw.tiendadiscos.entities.BillingInformationEntity;
+import co.edu.uniandes.csw.tiendadiscos.entities.TarjetaCreditoEntity;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Kevin Blanco
  */
-public class BillingInformationDetailDTO {
-    
+public class BillingInformationDetailDTO extends BillingInformationDTO implements Serializable {
+
+    // relación  cero o muchos tarjeta credito 
+    private List<TarjetaCreditoDTO> tarjetas;
+
+    public BillingInformationDetailDTO() {
+        super();
+    }
+
+    /**
+     * Constructor para transformar un Entity a un DTO
+     *
+     * @param billingInformationEntity La entidad de la cual se construye el DTO
+     */
+    public BillingInformationDetailDTO(BillingInformationEntity billingInformationEntity) {
+        super(billingInformationEntity);
+
+        if (billingInformationEntity.getTarjetas() != null) {
+            tarjetas = new ArrayList<>();
+
+            for (TarjetaCreditoEntity tarjeta : billingInformationEntity.getTarjetas()) {
+                tarjetas.add(new TarjetaCreditoDTO(tarjeta));
+            }
+        }
+    }
+
+    public BillingInformationEntity toEntity() {
+        BillingInformationEntity entity = super.toEntity();
+        if (tarjetas != null) {
+            List<TarjetaCreditoEntity> tarjetasEntity = new ArrayList<>();
+
+            for (TarjetaCreditoDTO tarjetaCreditoDTO : getTarjetas()) {
+                tarjetasEntity.add(tarjetaCreditoDTO.toEntity());
+            }
+
+            entity.setTarjetas(tarjetasEntity);
+        }
+        return entity;
+    }
+
+    public List<TarjetaCreditoDTO> getTarjetas() {
+        return tarjetas;
+    }
+
+    public void setTarjetas(List<TarjetaCreditoDTO> tarjetas) {
+        this.tarjetas = tarjetas;
+    }
 }
--- s3_tiendadiscos-back/src/main/java/co/edu/uniandes/csw/tiendadiscos/entities/BillingInformationEntity.java
+++ s3_tiendadiscos-back/src/main/java/co/edu/uniandes/csw/tiendadiscos/entities/BillingInformationEntity.java
@@ -6,11 +6,108 @@
 package co.edu.uniandes.csw.tiendadiscos.entities;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
 
 /**
  *
- * @author estudiante
+ * @author Kevin Blanco
  */
-public class BillingInformationEntity extends BaseEntity implements Serializable{
-    
+public class BillingInformationEntity extends BaseEntity implements Serializable {
+
+    /**
+     * numero de cuenta de ahorro
+     */
+    private String cuentaAhorro;
+
+    /**
+     * Dinero gastado
+     */
+    private Double spent;
+
+    /**
+     * dinero recibido
+     */
+    private Double recieved;
+
+    /**
+     * tarjetas de credito del Billing
+     */
+    //PODAM ??
+    @OneToMany(mappedBy = "no se", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<TarjetaCreditoEntity> tarjetas;
+
+    /**
+     * retorna cuenta de ahorro asociada al billing
+     *
+     * @return cuenta de ahorro asociada al billing
+     */
+    public String getCuentaAhorro() {
+        return cuentaAhorro;
+    }
+
+    /**
+     * retorna dinero gastado en el billing
+     *
+     * @return dinero gastado en el billing
+     */
+    public Double getSpent() {
+        return spent;
+    }
+
+    /**
+     * retorna dinero recibido en el billing
+     *
+     * @return dinero recibido en el billing
+     */
+    public Double getRecieved() {
+        return recieved;
+    }
+
+    /**
+     * modifica el numero de la cuenta de ahorro del billing
+     *
+     * @param cuentaAhorro nuevo numero de cuenta de ahorro
+     */
+    public void setCuentaAhorro(String cuentaAhorro) {
+        this.cuentaAhorro = cuentaAhorro;
+    }
+
+    /**
+     * modifica el dinero gastado en el billing
+     *
+     * @param spent nuevo valor del dinero gastado
+     */
+    public void setSpent(Double spent) {
+        this.spent = spent;
+    }
+
+    /**
+     * modifica dinero recibido en el Billing
+     *
+     * @param recieved nuevo valor del dinero recibido
+     */
+    public void setRecieved(Double recieved) {
+
+        this.recieved = recieved;
+    }
+
+    /**
+     * Devuelve las tarjetas de credito del billing.
+     *
+     * @return Lista de entidades de tipo Reseña
+     */
+    public List<TarjetaCreditoEntity> getTarjetas() {
+        return tarjetas;
+    }
+
+    /**
+     * modifica las tarjetas de credito del Billing
+     *
+     * @param tarjetas nuevas tarjetas para el Billing
+     */
+    public void setTarjetas(List<TarjetaCreditoEntity> tarjetas) {
+        this.tarjetas = tarjetas;
+    }
 }
