1cc2970d4076cba3960b0b95530f054e2eb3690a
Kevin Blanco <estudiante@ISIS2603-309.sis.virtual.uniandes.edu.co>
2018-08-29 01:07:16 -0500

    DTOS, DETAIl y ENTITY para billing y tarjeta de credito parece que esta todo terminado

--- s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/BillingInformationDTO.java
+++ s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/BillingInformationDTO.java
@@ -7,76 +7,137 @@ package co.edu.uniandes.csw.tiendadiscos.dtos;
 
 import co.edu.uniandes.csw.tiendadiscos.entities.BillingInformationEntity;
 import java.io.Serializable;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  *
- * @author estudiante
+ * @author Kevin Blanco
  */
 public class BillingInformationDTO  implements Serializable{
     
+    /**
+     * ID unico de BillingInformation
+     */
     private Long id;
+    
+    /**
+     * numero de cuenta de ahorro
+     */
     private String cuentaAhorro;
+    
+    /**
+     * Dinero gastado
+     */
     private Double spent;
+    
+    /**
+     * dinero recibido
+     */
     private Double recieved ;
     
     /**
-     * 
+     * constructor vacio
      */
     public BillingInformationDTO () {}
 
-     public BillingInformationDTO (BillingInformationEntity billingInformationlEntity) {
+    /**
+         * Construye una Billing Information apartir de la entity
+     * @param billingInformationlEntity 
+     */
+    public BillingInformationDTO (BillingInformationEntity billingInformationlEntity) {
         if (billingInformationlEntity != null) {
             this.id = billingInformationlEntity.getId();
-            //TERMINAR
-            
-           // this.name = billingInformationlEntity.getName();
+            this.cuentaAhorro = billingInformationlEntity.getCuentaAhorro();
+            this.recieved =billingInformationlEntity.getRecieved();
+            this.spent = billingInformationlEntity.getSpent();
         }
     }
     
     /**
-     * 
-     * @return 
+     * Retorna id del Billing 
+     * @return id del Billing 
      */
     public Long getId() {
         return id;
     }
-
+    
+    /**
+     * retorna cuenta de ahorro asociada al billing 
+     * @return cuenta de ahorro asociada al billing
+     */
     public String getCuentaAhorro() {
         return cuentaAhorro;
     }
 
+    /**
+     * retorna dinero gastado en el billing 
+     * @return dinero gastado en el billing 
+     */
     public Double getSpent() {
         return spent;
     }
-
+    
+    /**
+     * retorna dinero recibido en el billing 
+     * @return dinero recibido en el billing 
+     */
     public Double getRecieved() {
         return recieved;
     }
-
+    
+   /**
+    * modifica id 
+    * @param id nuevo id 
+    */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * modifica el numero de la cuenta de ahorro del billing 
+     * @param cuentaAhorro nuevo numero de cuenta de ahorro
+     */
     public void setCuentaAhorro(String cuentaAhorro) {
         this.cuentaAhorro = cuentaAhorro;
     }
-
+    
+    /**
+     * modifica el dinero gastado en el billing
+     * @param spent nuevo valor del dinero gastado
+     */
     public void setSpent(Double spent) {
         this.spent = spent;
     }
 
+    /**
+     * modifica dinero recibido en el Billing
+     * @param recieved nuevo valor del dinero recibido
+     */
     public void setRecieved(Double recieved) {
+        
         this.recieved = recieved;
     }
     
     
+     /**
+     * convierte DTO a Entity
+     *
+     * @return Entity con los valores del DTO
+     */
     public BillingInformationEntity toEntity () {
-   
-    return null;
+        BillingInformationEntity billingInformationEntity = new BillingInformationEntity();
+        billingInformationEntity.setId(this.id);
+        billingInformationEntity.setCuentaAhorro(this.getCuentaAhorro());
+        billingInformationEntity.setRecieved(this.getRecieved());
+        billingInformationEntity.setSpent(this.getSpent());
+        return billingInformationEntity;
     }
+    @Override
     public String toString() {
-    
-    return null;
+
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+
     }
    
     
