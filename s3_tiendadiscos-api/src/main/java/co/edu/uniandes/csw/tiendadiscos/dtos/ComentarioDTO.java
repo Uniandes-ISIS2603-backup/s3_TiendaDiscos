df47831ac0c29dd9c63ed7adfa29c99f96dbc7ba
Sebastian Martinez <estudiante@ISIS2603-298.sis.virtual.uniandes.edu.co>
2018-08-28 19:06:15 -0500

    Dtos terminados

--- s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/ComentarioDTO.java
+++ s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/ComentarioDTO.java
@@ -14,7 +14,9 @@ import javax.persistence.Id;
  */
 public class ComentarioDTO implements Serializable{
    
-    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     //Atributos
     private Long id;
     private String contenido;
