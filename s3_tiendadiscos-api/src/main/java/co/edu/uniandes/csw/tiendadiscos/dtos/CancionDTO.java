e9f088c297a4f1e4e907c4e2932a4b416547374b
Camilo Salinas <ca.salinas@uniandes.edu.co>
2018-09-13 09:39:09 -0500

    Bug removed

--- s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/CancionDTO.java
+++ s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/CancionDTO.java
@@ -186,12 +186,6 @@ public class CancionDTO implements Serializable
         return calificacion;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
 
     @Override
     public String toString() {
