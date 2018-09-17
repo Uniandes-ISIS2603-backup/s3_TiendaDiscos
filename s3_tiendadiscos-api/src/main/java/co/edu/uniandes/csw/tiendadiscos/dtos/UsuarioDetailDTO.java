4434cbf40cd7abe12bb0d68376f888cae91de769
Camilo Salinas <ca.salinas@uniandes.edu.co>
2018-09-17 02:35:15 -0500

    Cambios en el UsuarioDetailDTO

--- s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/UsuarioDetailDTO.java
+++ s3_tiendadiscos-api/src/main/java/co/edu/uniandes/csw/tiendadiscos/dtos/UsuarioDetailDTO.java
@@ -4,10 +4,11 @@
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.tiendadiscos.dtos;
-
+/*imports*/
+import co.edu.uniandes.csw.tiendadiscos.entities.ComentarioEntity;
+import co.edu.uniandes.csw.tiendadiscos.entities.TransaccionEntity;
 import co.edu.uniandes.csw.tiendadiscos.entities.UsuarioEntity;
 import co.edu.uniandes.csw.tiendadiscos.entities.ViniloEntity;
-import co.edu.uniandes.csw.tiendadiscos.entities.WishListEntity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +22,11 @@ public class UsuarioDetailDTO extends UsuarioDTO implements Serializable{
     /*Relación 0 a muchos vinilos*/
     private List<ViniloDTO> vinilos;
     /*Relación 0 a muchas transacciones*/
-    private List<TransaccionDTO> transacciones;
+    private List<TransaccionDTO> transaccionesRecibidas;
+    /*Relacion 0 a muchas transacciones*/
+    private List<TransaccionDTO> transaccionesGeneradas;
     /*Relación 0 a muchos comentarios*/
-    private List<ComentarioDTO> comentario;
+    private List<ComentarioDTO> comentarios;
     
 
     
@@ -49,15 +52,49 @@ public class UsuarioDetailDTO extends UsuarioDTO implements Serializable{
             {
                 vinilos.add(new ViniloDTO(actual));
             }
+        }
+        if(userEntity.getComentariosH()!= null){
+           comentarios = new ArrayList<>();
+            for(ComentarioEntity actual : userEntity.getComentariosH())
+            {
+                comentarios.add(new ComentarioDTO(actual));
+            }       
+        }
+        if(userEntity.getTransaccionesG()!= null){
+           transaccionesGeneradas = new ArrayList<>();
+            for(TransaccionEntity actual : userEntity.getTransaccionesG())
+            {
+                transaccionesGeneradas.add(new TransaccionDTO(actual));
+            }       
+        }
+        if(userEntity.getTransaccionesR()!= null){
+           transaccionesRecibidas = new ArrayList<>();
+            for(TransaccionEntity actual : userEntity.getTransaccionesR())
+            {
+                transaccionesRecibidas.add(new TransaccionDTO(actual));
+            }       
+        }
         
-    }
        
     
+        
     }
     public UsuarioEntity toEntity(){
          UsuarioEntity usuarioEntity =super.toEntity();
-        
-        
+         if(vinilos !=null){
+             List<ViniloEntity> vinilosEntity = new ArrayList<>();
+             for(ViniloDTO viniloDTO: vinilos){
+                 vinilosEntity.add(viniloDTO.toEntity());
+             }
+             usuarioEntity.setVinilos(vinilosEntity);
+         }
+        if(comentarios !=null){
+             List<ComentarioEntity> comentariosEntity = new ArrayList<>();
+             for(ComentarioDTO comentarioDTO: comentarios){
+                 comentariosEntity.add(comentarioDTO.toEntity());
+             }
+                 usuarioEntity.setComentariosH(comentariosEntity);
+         }
         
         
         
